9fd5c58db773ff4a663c68924a431c2d46a021b9
Edwin Montoya <emontoya@eafit.edu.co>
2020-03-31 07:10:07 -0500

    update mapreduce

--- 02-mapreduce/wordcount-mr.py
+++ 02-mapreduce/wordcount-mr.py
@@ -3,7 +3,8 @@ from mrjob.job import MRJob
 class MRWordFrequencyCount(MRJob):
 
     def mapper(self, _, line):
-        for w in line.decode('utf-8', 'ignore').split():
+#       for w in line.decode('utf-8', 'ignore').split():
+        for w in line.split():
             yield w,1
 
     def reducer(self, key, values):
